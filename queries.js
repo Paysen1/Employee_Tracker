b96e4c6a79fdb19a3842e306e348e6129134a1ac
Peak <pglund18@yahoo.com>
2023-07-28 11:59:52 -0500

    starting code

---
+++ queries.js
@@ -0,0 +1,16 @@
+const db = require('./db');
+
+// Define functions for various SQL queries
+const getAllDepartments = async () => {
+};
+
+// Implement similar functions for roles and employees
+
+const addDepartment = async (departmentName) => {
+};
+
+
+module.exports = {
+  getAllDepartments,
+
+};
